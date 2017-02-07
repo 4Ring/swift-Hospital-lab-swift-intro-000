32a27bd5acc8cac9e9ec9f11512b9d76ec0e8ead
4Ring <ukrainianslavic@gmail.com>
2017-02-06 23:19:01 -0800

    done

--- Hospital/SchoolEmployees.swift
+++ Hospital/SchoolEmployees.swift
@@ -3,8 +3,34 @@
 //  Hospital
 
 
+struct AdvisoryBoardMember {
+    var name: String
+    var vacationDays: Int = 30
+    
+    func wages() -> Double {
+        return 100_000.00
+    }
+}
 
+extension AdvisoryBoardMember: Employee { }
 
+struct Principle {
+    var name: String
+    var vacationDays: Int = 20
+    
+    func wages() -> Double {
+        return 80_000.00
+    }
+}
+
+extension Principle: Employee, Reprimand { }
+
+struct Teacher {
+    var name: String
+    var vacationDays: Int = 15
+}
+
+extension Teacher: Employee, Teach { }
 
 
 
